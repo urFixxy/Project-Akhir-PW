d73c52bae9691e207ab00d51e2a904e7ff429dc8
fit <fitdafan@gmail.com>
2025-06-11 10:23:51 +0700

    masih hampir

--- query.sql
+++ query.sql
@@ -6,14 +6,14 @@ CREATE TABLE kategori (
 );
 
 CREATE TABLE obat (
-  kode_obat VARCHAR(50) PRIMARY KEY,
-  nama_obat VARCHAR(100) NOT NULL,
-  stok_obat INTEGER DEFAULT 0,
-  harga_obat NUMERIC(12, 2),
-  kode_kategori VARCHAR(50) NOT NULL REFERENCES kategori(kode_kategori),
-  kode_supplier VARCHAR(50) NOT NULL REFERENCES supplier(kode_supplier),
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    kode_obat VARCHAR(50) PRIMARY KEY,
+    nama_obat VARCHAR(100) NOT NULL,
+    stok_obat INTEGER DEFAULT 0,
+    harga_obat NUMERIC(12, 2),
+    kode_kategori VARCHAR(50) NOT NULL REFERENCES kategori(kode_kategori),
+    kode_supplier VARCHAR(50) NOT NULL REFERENCES supplier(kode_supplier),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE supplier (
@@ -35,3 +35,14 @@ CREATE TABLE pembelian (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE pengeluaran (
+    kode_keluar VARCHAR(20) PRIMARY KEY,
+    kode_obat VARCHAR(20) NOT NULL REFERENCES obat(kode_obat),
+    jumlah INT NOT NULL,
+    tanggal DATE NOT NULL,
+    kode_pegawai VARCHAR(20) NOT NULL REFERENCES pegawai(kode_pegawai),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
